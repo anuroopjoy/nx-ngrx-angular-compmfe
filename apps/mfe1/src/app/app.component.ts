8bd7e59c26b4a3dfe962a1555fc38a6a9e68ab11
Anuroop <anuroopjoy@gmail.com>
2021-09-20 22:43:26 +0530

    mfe

--- apps/mfe1/src/app/app.component.ts
+++ apps/mfe1/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  DataService,
+  getCategories,
+  setSelectedCategory,
+} from '@nx-ngrx-angular/data-store';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mfe1';
+  constructor(private readonly store: Store, private dataService: DataService) {
+    const categories = this.dataService.getCategories();
+    this.store.dispatch(getCategories({ categories }));
+    if (categories?.length) {
+      this.store.dispatch(setSelectedCategory({ position: 0 }));
+    }
+  }
 }
